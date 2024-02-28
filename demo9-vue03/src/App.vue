4772c41a04eb8887f269248b13dffae79eb08e0c
kwg <1020296242@qq.com>
2024-02-28 15:55:57 +0800

    vue about

---
+++ demo9-vue03/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { ref } from "vue";
+  let counter = ref(0);
+  function increment() {
+    counter.value += 1;
+  }
+  function decrement() {
+    counter.value -= 1;
+  }
+</script>
+
+<template>
+  <div>
+    <button @click="decrement()">-</button>
+    <span v-text="counter"></span>
+    <button @click="increment()">+</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
